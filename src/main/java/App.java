ae71e78fad3661bcb08fc9c5bba256a6800cb68a
Frank Uzoka <Frankuzoka@hotmail.com>
2020-05-06 10:57:43 -0400

    Add logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,15 +4,20 @@ public class App {
 
     public static void main(String[] args) {
 
+        logic();
     }
 
     static void logic(){
         System.out.println("SQUARE ROOT!\n");
+
+        System.out.print("Enter a number: ");
 
         Scanner scan = new Scanner(System.in);
 
         int userInput = scan.nextInt();
 
         double squareRoot = Math.sqrt(userInput);
+
+        System.out.println(squareRoot);
     }
 }
